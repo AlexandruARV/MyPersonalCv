ffa3fd98dc0423074518f235385bcfe43dddce60
AlexandruARV <162844302+AlexandruARV@users.noreply.github.com>
2024-03-27 19:18:53 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,46 @@
 import { generateSkills } from "./usefulScripts/generateSkills.js";
 
+// header Nav - comp
+const allNavLinks = document.querySelectorAll(".nav__link");
+const navList = document.querySelector(".nav_list");
+const nav = document.querySelector(".nav");
+
+// Skills - comp
 const navSkills = document.querySelector(".skills__flex");
 const showSkills = document.querySelectorAll(".show__skills");
 const arrows = document.querySelectorAll(".btn__arrow");
 
+// Slider- comp
+const btnLeft = document.querySelector(".slider__btn--left");
+const btnRight = document.querySelector(".slider__btn--right");
+const slides = document.querySelectorAll(".slide");
+const slider = document.querySelector(".slider");
+const dotsContainer = document.querySelector(".dots");
+
+// NavBar effects
+
+function handleHover(e) {
+  if (e.target.classList.contains("nav__link")) {
+    const currentEl = e.target;
+    const siblings = currentEl
+      .closest(".nav_list")
+      .querySelectorAll(".nav__link");
+    siblings.forEach((el) => {
+      if (el !== currentEl) {
+        currentEl.style.color = this;
+      }
+    });
+  }
+}
+
+navList.addEventListener("mouseover", handleHover.bind("purple"));
+navList.addEventListener("mouseout", handleHover.bind("black"));
+
 const scrollingAnimation = function () {
   document.querySelector(".nav_list").addEventListener("click", (e) => {
-    if (e.target.getAttribute("class") === "nav__link") {
+    if (e.target.closest(".nav__link")?.getAttribute("class") === "nav__link") {
       e.preventDefault();
-      const id = e.target.getAttribute("href");
+      const id = e.target.closest(".nav__link").getAttribute("href");
       const section = document.querySelector(`${id}`);
       section.scrollIntoView({ behavior: "smooth" });
     }
@@ -41,6 +73,28 @@ navSkills.addEventListener("click", (e) => {
   clickedID = clicked.id;
 });
 
+const sectionHome = document.querySelector(".section-home");
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  console.log(entry);
+  if (!entry.isIntersecting) {
+    nav.classList.add("sticky");
+    console.log(nav);
+  } else {
+    nav.classList.remove("sticky");
+  }
+};
+
+const homeObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  treshold: 0,
+  rootMargin: `-90px`,
+});
+homeObserver.observe(sectionHome);
+
+// Skills disp
+
 const getSkills = async function (url) {
   const res = await fetch(`${url}`);
   const data = await res.json();
@@ -53,12 +107,7 @@ const getSkills = async function (url) {
 
 getSkills(`./skills.json`);
 
-const btnLeft = document.querySelector(".slider__btn--left");
-const btnRight = document.querySelector(".slider__btn--right");
-const slides = document.querySelectorAll(".slide");
-const slider = document.querySelector(".slider");
-
-const dotsContainer = document.querySelector(".dots");
+// Slider animation
 
 const maxSlides = slides.length;
 let curSlide = 0;
@@ -129,36 +178,13 @@ dotsContainer.addEventListener("click", (e) => {
   }
 });
 
-const nav = document.querySelector(".nav");
-
-const sectionHome = document.querySelector(".section-home");
-
-console.log(sectionHome);
-
-const stickyNav = function (entries) {
-  const [entry] = entries;
-  if (!entry.isIntersecting) {
-    nav.classList.add("sticky");
-  } else {
-    nav.classList.remove("sticky");
-  }
-  console.log(entry);
-};
-
-const homeObserver = new IntersectionObserver(stickyNav, {
-  root: null,
-  treshold: 0,
-  rootMargin: `-90px`,
-});
-homeObserver.observe(sectionHome);
+// Mobile menu
 
 const mobileMenu = document.querySelector(".menu--mobile");
 const listMenu = document.querySelector(".nav_list");
 const imgMenuMobile = document.querySelector(".menu__mobile__img");
 let isClicked = true;
 mobileMenu.addEventListener("click", (e) => {
-  console.log(imgMenuMobile.src);
-
   if (isClicked) {
     listMenu.classList.add("mobile__nav__list");
     imgMenuMobile.src = `./photos/pics-mobile/times.svg`;
